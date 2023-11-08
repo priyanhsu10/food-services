755eb4d51580c6abd74c56e96de388dada6ff9ab
priyanshu10 <priyanshuparate@gmail.com>
2023-11-08 22:43:41 +0530

    initial services

---
+++ order-tracking-service/src/main/java/com/foody/ordertrackingservice/OrderTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.foody.ordertrackingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTrackingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderTrackingServiceApplication.class, args);
+	}
+
+}
