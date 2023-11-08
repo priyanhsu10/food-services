3f19261acb5ba45d82f8479cc0a39daae7a9df35
priyanshu10 <priyanshuparate@gmail.com>
2023-11-08 22:48:10 +0530

    restorent service

---
+++ restorentservice/src/main/java/com/foody/restorentservice/RestorantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.foody.restorentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestorantServiceApplication.class, args);
+	}
+
+}
---
+++ restorentservice/src/test/java/com/foody/restorentservice/RestorantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foody.restorentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
